f5e20eda3449ecf7266f7e1f06e51b6a4d8e0746
RonBenGal <117488341+RonBenGal@users.noreply.github.com>
2024-09-10 17:50:28 +0300

    first commit

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,65 @@
+import { Navigate,useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import api from "../api";
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+import { useState, useEffect } from "react";
+
+function ProtectedRoute({ children }) {
+  const [isAuthorized, setIsAuthorized] = useState(null);
+  
+  useEffect(() => {
+    auth().catch(() => setIsAuthorized(false));
+  }, []);
+
+  const refreshToken = async () => {
+    const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+    if (!refreshToken) {
+      setIsAuthorized(false);
+      return;
+    }
+    try {
+      const response = await api.post("/api/token/refresh/", {
+        refresh: refreshToken,
+      });
+      if (response.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, response.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      setIsAuthorized(false);
+      console.log("Refresh token error:", error);
+    }
+  };
+  
+  const auth = async () => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (!token) {
+      setIsAuthorized(false);
+      return;
+    }
+    try {
+    const decoded = jwtDecode(token);
+    const tokenExpiration = decoded.exp;
+    const now = Date.now() / 1000;
+
+    if (tokenExpiration < now) {
+      await refreshToken();
+    } else {
+      setIsAuthorized(true);
+    }
+  } catch (error) {
+    setIsAuthorized(false);
+    console.error("Token decode error:", error);
+  }
+  };
+
+  if (isAuthorized === null) {
+    return <div>Loading...</div>;
+  }
+
+  return isAuthorized ? children : <Navigate to="/login" />;
+}
+
+export default ProtectedRoute;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api";
+import Note from "../components/Note"
+import "../styles/Home.css"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
+
+function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  
+  const navigate = useNavigate();  // Get the navigate function from react-router-dom
+
+  // Function to handle logout and clear tokens
+  const handleLogout = () => {
+    localStorage.removeItem(ACCESS_TOKEN);  // Remove access token
+    localStorage.removeItem(REFRESH_TOKEN);  // Remove refresh token
+    navigate('/login');  // Redirect to login page
+  };
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted!");
+        else alert("Failed to delete note.");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+    
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Note created!");
+        else alert("Failed to make note");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+    
+  };
+
+  return (
+    <div>
+        <div>
+            <h2>Notes</h2>
+            {notes.map((note) => <Note note={note} onDelete={deleteNote} key={note.id} />)}
+            
+        </div>
+        <h2>Create a Note</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+            <br />
+            <input
+                type="text"
+                id="title"
+                name="title"
+                required
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+            />
+            <label htmlFor="content">Content:</label>
+            <br />
+            <textarea
+                id="content"
+                name="content"
+                required
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+            <br />
+            <input type="submit" value="Submit"></input>
+        </form>
+        <button className="logout-button" onClick={handleLogout}>Log Out</button>
+    </div>
+);
+}
+
+export default Home;
